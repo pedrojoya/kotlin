39dc40d4e7a9c13291a4a4c5175c2fe6a8e4346e
pedrojoya <pedrojoya@gmail.com>
2018-12-23 08:43:35 +0100

    PR148 - Upgrade dependencies

--- PR148-FloatingActionButton/app/src/main/java/es/iessaladillo/pedrojoya/pr148/ui/main/MainActivity.kt
+++ PR148-FloatingActionButton/app/src/main/java/es/iessaladillo/pedrojoya/pr148/ui/main/MainActivity.kt
@@ -2,7 +2,10 @@ package es.iessaladillo.pedrojoya.pr148.ui.main
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import es.iessaladillo.pedrojoya.pr148.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,7 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+        if (savedInstanceState == null) {
+            loadInitialFragment()
+        }
+    }
+
+    private fun setupViews() {
         setupToolbar()
+        setupFab()
     }
 
     private fun setupToolbar() {
@@ -20,9 +31,35 @@ class MainActivity : AppCompatActivity() {
         collapsingToolbarLayout.title = title
     }
 
+    private fun setupFab() {
+        fab.setOnClickListener { save() }
+    }
+
+    private fun loadInitialFragment() {
+        supportFragmentManager.commit {
+            replace(R.id.flContent, MainFragment.newInstance(), MainFragment::class.java.simpleName)
+        }
+    }
+
+    private fun save() {
+        Toast.makeText(this, getString(R.string.main_fab_clicked), Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.activity_main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.mnuSettings) {
+            showSettings()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showSettings() {
+        Toast.makeText(this, getString(R.string.main_mnuSettings), Toast.LENGTH_SHORT).show()
     }
 
 }
