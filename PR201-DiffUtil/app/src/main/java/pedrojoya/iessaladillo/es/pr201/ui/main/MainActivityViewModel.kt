1a211dcf96eaf6cdbcc975efea31b3f9fc2e7206
pedrojoya <pedrojoya@gmail.com>
2018-12-25 13:30:05 +0100

    PR201 - Use ktx for Transformations.map and switchMap

--- PR201-DiffUtil/app/src/main/java/pedrojoya/iessaladillo/es/pr201/ui/main/MainActivityViewModel.kt
+++ PR201-DiffUtil/app/src/main/java/pedrojoya/iessaladillo/es/pr201/ui/main/MainActivityViewModel.kt
@@ -1,9 +1,6 @@
 package pedrojoya.iessaladillo.es.pr201.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import pedrojoya.iessaladillo.es.pr201.data.Repository
 import pedrojoya.iessaladillo.es.pr201.data.local.model.Student
 
@@ -15,21 +12,18 @@ class MainActivityViewModel(private val repository: Repository) :
     private var emptyListLiveData: LiveData<Boolean>
 
     init {
-        studentsLiveData = Transformations.switchMap(descLiveData) { desc -> repository
-                .queryStudentsOrderedByName(desc) }
-        emptyListLiveData = Transformations.map(studentsLiveData) {
-            students -> students == null || students.isEmpty()
-        }
+        studentsLiveData = descLiveData.switchMap { desc -> repository.queryStudentsOrderedByName(desc) }
+        emptyListLiveData = studentsLiveData.map { students -> students.isEmpty() }
         descLiveData.postValue(false)
     }
 
     fun toggleOrder() {
-        descLiveData.postValue(!(descLiveData.value?:true))
+        descLiveData.postValue(!(descLiveData.value ?: true))
     }
 
     fun isListEmpty(): LiveData<Boolean> = emptyListLiveData
 
-    fun isInDescendentOrder(): Boolean =  descLiveData.value?:false
+    fun isInDescendentOrder(): Boolean = descLiveData.value ?: false
 
     fun getStudents(): LiveData<List<Student>> = studentsLiveData
 
