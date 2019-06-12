c45147ed128c4a1da634487f27baa9a20618f781
pedrojoya <pedrojoya@gmail.com>
2019-06-12 22:35:39 +0200

    PR004 - Clean up

--- PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/pr004/extensions/IntentExt.kt
+++ PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/pr004/extensions/IntentExt.kt
@@ -4,35 +4,10 @@ package es.iessaladillo.pedrojoya.pr004.extensions
 
 import android.app.SearchManager
 import android.content.ContentResolver.SCHEME_CONTENT
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.provider.Settings
 import android.webkit.WebView.SCHEME_GEO
 import android.webkit.WebView.SCHEME_TEL
-
-fun Intent.isActivityAvailable(context: Context) =
-        context.applicationContext.packageManager.queryIntentActivities(this,
-                PackageManager.MATCH_DEFAULT_ONLY).size > 0
-
-inline fun Context.startActivityWithCheck(intent: Intent, error: () -> Unit = {}) {
-    if (intent.isActivityAvailable(this.applicationContext)) {
-        startActivity(intent)
-    } else {
-        error.invoke()
-    }
-}
-
-// Apps settings activity intent.
-fun newInstalledAppDetailsActivityIntent(context: Context): Intent =
-        Intent()
-                .setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                .addCategory(Intent.CATEGORY_DEFAULT)
-                .setData(Uri.fromParts("package", context.packageName, null))
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
 
 fun newViewUriIntent(uri: Uri) = Intent(Intent.ACTION_VIEW, uri)
 
@@ -50,5 +25,3 @@ fun newSearchInMapIntent(text: String) = Intent(Intent.ACTION_VIEW,
 
 fun newContactsIntent() = Intent(Intent.ACTION_VIEW, Uri.parse
 ("$SCHEME_CONTENT://contacts/people/"))
-
-fun newCallIntent(phoneNumber: String) = Intent(Intent.ACTION_CALL, Uri.parse("$SCHEME_TEL$phoneNumber"))
--- PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/pr004/ui/main/MainActivity.kt
+++ PR004-IntentsImplicitos/app/src/main/java/es/iessaladillo/pedrojoya/pr004/ui/main/MainActivity.kt
@@ -1,10 +1,9 @@
 package es.iessaladillo.pedrojoya.pr004.ui.main
 
-import android.Manifest
+import android.content.ActivityNotFoundException
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import es.iessaladillo.pedrojoya.pr004.R
 import es.iessaladillo.pedrojoya.pr004.extensions.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,20 +15,18 @@ private const val LONGITUDE = 36.1121
 private const val LATITUDE = -5.44347
 private const val ZOOM = 19
 private const val MAP_SEARCH_TEXT = "duque de rivas, Algeciras"
-private const val RP_CALL = 1
 
 class MainActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initViews()
+        setupViews()
     }
 
-    private fun initViews() {
+    private fun setupViews() {
         btnShowInBrowser.setOnClickListener { showInBrowser(WEB_URL) }
         btnSearch.setOnClickListener { search(SEARCH_TEXT) }
-        btnCall.setOnClickListener { wantsToCall(PHONE_NUMBER) }
         btnDial.setOnClickListener { dial(PHONE_NUMBER) }
         btnShowInMap.setOnClickListener { showInMap(LONGITUDE, LATITUDE, ZOOM) }
         btnSearchInMap.setOnClickListener { searchInMap(MAP_SEARCH_TEXT) }
@@ -37,77 +34,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showInBrowser(url: String) {
-        startActivityWithCheck(newViewUriIntent(Uri.parse(url))) {
-            toast(R.string.main_activity_no_web_browser)
+        val intent = newViewUriIntent(Uri.parse(url))
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_web_browser)
         }
     }
 
     private fun search(text: String) {
-        startActivityWithCheck(newWebSearchIntent(text)) {
-            toast(R.string.main_activity_no_web_search)
-        }
-    }
-
-    private fun wantsToCall(phoneNumber: String) {
-        if (!hasPermission(Manifest.permission.CALL_PHONE)) {
-            requestCallPermission()
-        } else {
-            call(phoneNumber)
-        }
-    }
-
-    private fun requestCallPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), RP_CALL)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (requestCode == RP_CALL && hasPermission(Manifest.permission.CALL_PHONE)) {
-            call(PHONE_NUMBER)
-        } else {
-            // Check if the user set "Don't ask again"
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.CALL_PHONE)) {
-                reportRationale()
-            } else {
-                btnSearch.snackbar(R.string.main_activity_no_call_permission_rationale)
-            }
-        }
-    }
-
-    private fun reportRationale() {
-        btnSearch.snackbar(R.string.general_permission_required, getString(R.string.general_configure)) {
-            startActivity(newInstalledAppDetailsActivityIntent(this@MainActivity))
-        }
-    }
-
-    private fun call(phoneNumber: String) {
-        startActivityWithCheck(newCallIntent(phoneNumber)) {
-            toast(R.string.main_activity_no_call_app)
+        val intent = newWebSearchIntent(text)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_web_search)
         }
     }
 
     private fun dial(phoneNumber: String) {
-        startActivityWithCheck(newDialIntent(phoneNumber)) {
-            toast(R.string.main_activity_no_dial_app)
+        val intent = newDialIntent(phoneNumber)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_dial_app)
         }
     }
 
     private fun showInMap(longitude: Double, latitude: Double, zoom: Int) {
-        startActivityWithCheck(newShowInMapIntent(longitude, latitude, zoom)) {
-            toast(R.string.main_activity_no_maps_app)
+        val intent = newShowInMapIntent(longitude, latitude, zoom)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_maps_app)
         }
     }
 
     private fun searchInMap(text: String) {
-        startActivityWithCheck(newSearchInMapIntent(text)) {
-            toast(R.string.main_activity_no_maps_app)
+        val intent = newSearchInMapIntent(text)
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_maps_app)
         }
     }
 
     private fun showContacts() {
-        startActivityWithCheck(newContactsIntent()) {
-            toast(R.string.main_activity_no_contacts_app)
+        val intent = newContactsIntent()
+        try {
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            toast(R.string.main_no_contacts_app)
         }
     }
 
